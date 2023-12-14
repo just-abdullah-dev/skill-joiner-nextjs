bf4ac9789fa54e60fbf0350a8c269ed93bf6e9a2
Abdullah <126217821+just-abdullah-dev@users.noreply.github.com>
2023-12-14 15:18:57 +0500

    Update mail.js

--- src/pages/api/superAdmin/mail.js
+++ src/pages/api/superAdmin/mail.js
@@ -1,6 +1,6 @@
 import { sendOtp } from "@/utils/sendOtp";
 
 export default async function (req, res){
-    const otp = sendOtp('b22f1231se145@fecid.paf-iast.edu.pk');
+    const otp = sendOtp('abbasmkmushtaq6@gmail.com');
     res.send(`Email has been sent!. Code is : ${otp}`)
 }
