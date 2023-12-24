84a2a128fe3c17cb9e84095551f358369421e9a3
just-abdullah-dev <abdulhaseeb9274@gmail.com>
2023-12-24 11:15:51 +0500

    getProfile Setup

--- src/pages/api/users/getProfile.js
+++ src/pages/api/users/getProfile.js
@@ -19,12 +19,9 @@ export default async function handler(req, res) {
         }
         
         await connectDB();
-
-        // await userAuthGuard(req, res);
+        await userAuthGuard(req, res);
         
-        
-        // const user = await User.findById(req.user?._id)
-        const user = await User.findById('657b3212a1790f85bebcf2db')
+        const user = await User.findById(req.user?._id)
       .select('-password')
       .populate([
         {
